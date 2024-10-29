1fa1baac816e952b54c908a2567de936d3c948c0
yuiseki <yuiseki@gmail.com>
2024-10-29 22:17:52 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { FileEditor } from "./components/FileEditor";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>charites-like-opfs</h1>
+        <FileEditor />
+      </div>
+    </>
+  );
+}
+
+export default App;
