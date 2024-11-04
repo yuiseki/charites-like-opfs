1250f0c880e7f5f2a98a9a09e26bd8c985cea821
yuiseki <yuiseki@gmail.com>
2024-11-04 12:10:09 +0900

    lint通す

--- src/components/FileEditor.tsx
+++ src/components/FileEditor.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useCallback, useEffect, useState } from "react";
 import CodeMirror from "@uiw/react-codemirror";
-import { color, colorView, colorTheme } from "@uiw/codemirror-extensions-color";
+import { colorView, colorTheme } from "@uiw/codemirror-extensions-color";
 import { darcula } from "@uiw/codemirror-theme-darcula";
 import { yaml } from "@codemirror/lang-yaml";
 import { useKeyBind } from "../hooks/keybind";
